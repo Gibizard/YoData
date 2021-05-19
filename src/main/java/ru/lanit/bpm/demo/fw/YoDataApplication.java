cc67065c62d27bcc3e23edfc7741bef0ae7918c5
DGiba <DGiba@lanit.ru>
2021-05-19 11:18:18 +0300

    Initial commit

---
+++ src/main/java/ru/lanit/bpm/demo/fw/YoDataApplication.java
@@ -0,0 +1,12 @@
+package ru.lanit.bpm.demo.fw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoDataApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/lanit/bpm/demo/fw/YoDataApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.lanit.bpm.demo.fw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = YoDataApplication.class)
+class YoDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
