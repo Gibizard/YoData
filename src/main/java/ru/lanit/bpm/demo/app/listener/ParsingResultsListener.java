756cd0a8428409152b5dc47cfee7781c56a55bd8
DGiba <DGiba@lanit.ru>
2021-10-27 19:33:16 +0300

    + Kafka

--- src/main/java/ru/lanit/bpm/demo/app/listener/ParsingResultsListener.java
+++ src/main/java/ru/lanit/bpm/demo/app/listener/ParsingResultsListener.java
@@ -2,10 +2,12 @@ package ru.lanit.bpm.demo.app.listener;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 import ru.lanit.bpm.demo.adapter.kafka.ParsingResultToSend;
 import ru.lanit.bpm.demo.adapter.telegram.TelegramAdapter;
 
 @RequiredArgsConstructor
+@Service
 public class ParsingResultsListener {
     TelegramAdapter telegramAdapter;
 
